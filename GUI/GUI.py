18aa9ded72de09d1cf90aa0d40196232db813691
Ayaan Shaikh <ayaanmaths04@gmail.com>
2023-12-05 17:14:49 +0530

    Add Events table and insert dummy data

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,48 +1,267 @@
-import tkinter
-import mysql.connector
+import tkinter as tk
+import mysql.connector as m
 
-# Connect to database
+(WIDTH, HEIGHT) = (500, 500)
+
+root = tk.Tk()
+root.title("Events Management System")
+root.geometry("500x500")
+
+small_font = ("Helvetica", 10)
+font = ("Helvetica", 14)
+heading_font = ("Helvetica", 18)
+button_bg = "#4CAF50"
+button_fg = "#FFFFFF"
+text = "#212121"
+
+def destroy():
+	for i in root.winfo_children():
+		i.destroy()
+
 def connect_db():
-	mydb = mysql.connector.connect(
+	db = m.connect(
 		host="localhost",
 		user="root",
-		password="root",
-		database="Library"
+		passwd="root",
+		database="Events"
 	)
-	return mydb
+	return db
 
-def display_books(mydb):
-	mycursor = mydb.cursor()
-	mycursor.execute("select * from Books")
-	myresult = mycursor.fetchall()
-	return myresult
+def select_db(attr = "*"):
+	db = connect_db()
+	cursor = db.cursor()
+	cursor.execute("SELECT {attr} FROM Events".format(attr=attr))
+	result = cursor.fetchall()
+	return result
 
-# Create window
-def frame1():
-	window = tkinter.Tk()
-	window.title("Library")
-	database = connect_db()
-	books = display_books(database)
-	
-	# Create label
-	label = tkinter.Label(window, text="Library", font=("Arial Bold", 20))
-	label.grid(column=0, row=0)
-	
-	# Show books 10 at a time
-	for i in range(10):
-		book = tkinter.Label(window, text=books[i])
-		book.grid(column=0, row=i+1)
-	
-	# Pagination
-	# Create label
-	label = tkinter.Label(window, text="Page 1", font=("Arial Bold", 20))
-	label.grid(column=0, row=11)
-	
-	# Create button
-	button = tkinter.Button(window, text="Add book", command=frame2)
-	window.mainloop()
+def insert_db(title, description, date, fees):
+	db = connect_db()
+	cursor = db.cursor()
+	cursor.execute("INSERT INTO Events (title, description, date, fees) VALUES (%s, %s, %s, %s)", (title, description, date, fees))
+	db.commit()
+	print(cursor.rowcount, "record(s) inserted.")
 
-def frame2():
-	pass
+def update_db(price, id):
+	db = connect_db()
+	cursor = db.cursor()
+	cursor.execute("UPDATE Events SET fees = %s WHERE id = %s", (price, id))
+	db.commit()
+	print(cursor.rowcount, "record(s) updated.")
 
-frame1()
+def delete_db(id):
+	db = connect_db()
+	cursor = db.cursor()
+	cursor.execute("DELETE FROM Events WHERE id = %s", (id,))
+	db.commit()
+	print(cursor.rowcount, "record(s) deleted.")
+
+def checkNewEvent(title, description, date, fees):
+	if title and description and date and fees:
+		insert_db(title, description, date, fees)
+	else:
+		print("Please fill all the fields")
+
+def checkUpdateEvent(price, id):
+	if price:
+		update_db(price, id)
+	else:
+		print("Please fill all the fields")
+
+def updateEvent():
+	destroy()
+	updateEvent_frame = tk.Frame(root, width=WIDTH, height=HEIGHT)
+	updateEvent_frame.pack()
+
+	# Ids of Events
+	event_details = select_db("id, title")
+	for event in event_details:
+		(id, title) = event
+		# Id
+		event_id = tk.Label(updateEvent_frame, text=id, font=small_font)
+		event_id.grid(row=id, column=0)
+
+		# Title
+		event_title = tk.Label(updateEvent_frame, text=title, font=small_font)
+		event_title.grid(row=id, column=1)
+
+	# Form
+	form = tk.Frame(updateEvent_frame)
+	form.grid(padx=10, pady=10, columnspan=2)
+
+	# Id
+	id_label = tk.Label(form, text="Enter Id: ", font=font)
+	id_label.grid(row=0, column=0, sticky=tk.E)
+
+	id_entry = tk.Entry(form, font=font)
+	id_entry.grid(row=0, column=1, sticky=tk.E)
+
+	# Price
+	price_label = tk.Label(form, text="Enter new Price: ", font=font)
+	price_label.grid(row=1, column=0, sticky=tk.E)
+
+	price_entry = tk.Entry(form, font=font)
+	price_entry.grid(row=1, column=1, sticky=tk.E)
+
+	# Buttons
+	btn_update = tk.Button(updateEvent_frame, text="Update", font=font, bg=button_bg, fg=button_fg, command=lambda: checkUpdateEvent(price_entry.get(), id_entry.get()))
+	btn_update.grid(columnspan=2, padx=10, pady=10, ipadx=10, ipady=10)
+
+	btn_back = tk.Button(updateEvent_frame, text="Back", font=font, bg=button_bg, fg=button_fg, command=landing_page)
+	btn_back.grid(columnspan=2, padx=10, pady=10, ipadx=10, ipady=10)
+
+def showEvent():
+	destroy()
+	# Show Event
+	show_frame = tk.Frame(root, width=WIDTH, height=HEIGHT)
+	show_frame.pack()
+
+	# Title
+	title = tk.Label(show_frame, text="Events", font=heading_font)
+	title.pack(padx=10, pady=10)
+	
+	# Event Card
+	events = select_db()
+	for event in events:
+		(id, title, description, date, fees) = event
+		event_card = tk.Frame(show_frame, width=WIDTH, height=HEIGHT, bd=1, relief=tk.SOLID)
+		event_card.pack(padx=10, pady=10, ipadx=10, ipady=10)
+
+		# Title
+		event_title = tk.Label(event_card, text=title, font=font)
+		event_title.pack(anchor=tk.W)
+
+		# Description
+		event_description = tk.Label(event_card, text=description, font=font)
+		event_description.pack(side=tk.LEFT, )
+
+		# Date
+		event_date = tk.Label(event_card, text=date, font=font)
+		event_date.pack(side=tk.LEFT, )
+
+		event_fees_label = tk.Label(event_card, text="Fees: ", font=font)
+		event_fees_label.pack(side=tk.LEFT)
+
+		# Fees
+		event_fees = tk.Label(event_card, text=fees, font=font)
+		event_fees.pack(side=tk.LEFT)
+
+	# Buttons
+	btn_back = tk.Button(show_frame, text="Back", font=font, bg=button_bg, fg=button_fg, command=landing_page)
+	btn_back.pack(padx=10, pady=10, ipadx=10, ipady=10)
+
+def newEvent():
+	destroy()
+	newEvent_frame = tk.Frame(root, width=WIDTH, height=HEIGHT)
+	newEvent_frame.pack()
+
+	# Title
+	title = tk.Label(newEvent_frame, text="Add a new Event", font=heading_font)
+	title.pack(padx=10, pady=10)
+
+	# Form
+	form = tk.Frame(newEvent_frame)
+	form.pack(padx=10, pady=10)
+
+	# Title
+	title_label = tk.Label(form, text="Title: ", font=font)
+	title_label.grid(row=0, column=0, sticky=tk.E)
+
+	title_entry = tk.Entry(form, font=font)
+	title_entry.grid(row=0, column=1, sticky=tk.E)
+
+	# Description
+	description_label = tk.Label(form, text="Description: ", font=font)
+	description_label.grid(row=1, column=0, sticky=tk.E)
+
+	description_entry = tk.Entry(form, font=font)
+	description_entry.grid(row=1, column=1, sticky=tk.E)
+
+	# Date
+	date_label = tk.Label(form, text="Date: ", font=font)
+	date_label.grid(row=2, column=0, sticky=tk.E)
+
+	date_entry = tk.Entry(form, font=font)
+	date_entry.grid(row=2, column=1, sticky=tk.E)
+
+	# Fees
+	fees_label = tk.Label(form, text="Fees: ", font=font)
+	fees_label.grid(row=3, column=0, sticky=tk.E)
+
+	fees_entry = tk.Entry(form, font=font)
+	fees_entry.grid(row=3, column=1, sticky=tk.E)
+
+	# Buttons
+	btn_add = tk.Button(newEvent_frame, text="Add", font=font, bg=button_bg, fg=button_fg, command=lambda: checkNewEvent(title_entry.get(), description_entry.get(), date_entry.get(), fees_entry.get()))
+	btn_add.pack(padx=10, pady=10, ipadx=10, ipady=10)
+
+	btn_back = tk.Button(newEvent_frame, text="Back", font=font, bg=button_bg, fg=button_fg, command=landing_page)
+	btn_back.pack(padx=10, pady=10, ipadx=10, ipady=10)
+
+def deleteEvent():
+	destroy()
+	deleteEvent_frame = tk.Frame(root, width=WIDTH, height=HEIGHT)
+	deleteEvent_frame.pack()
+
+	# Ids of Events
+	event_details = select_db("id, title")
+	for event in event_details:
+		(id, title) = event
+		# Id
+		event_id = tk.Label(deleteEvent_frame, text=id, font=small_font)
+		event_id.grid(row=id, column=0)
+
+		# Title
+		event_title = tk.Label(deleteEvent_frame, text=title, font=small_font)
+		event_title.grid(row=id, column=1)
+
+	# Title
+	title = tk.Label(deleteEvent_frame, text="Delete an Event", font=heading_font)
+	title.grid(columnspan=2, padx=10, pady=10)
+
+	# Form
+	form = tk.Frame(deleteEvent_frame)
+	form.grid(columnspan=2, padx=10, pady=10)
+
+	# Id
+	id_label = tk.Label(form, text="Enter Id: ", font=font)
+	id_label.grid(columnspan=2, row=0, column=0, sticky=tk.E)
+
+	id_entry = tk.Entry(form, font=font)
+	id_entry.grid(columnspan=2, row=0, column=1, sticky=tk.E)
+
+	# Buttons
+	btn_delete = tk.Button(deleteEvent_frame, text="Delete", font=font, bg=button_bg, fg=button_fg, command=lambda: deleteEvent(id_entry.get()))
+	btn_delete.grid(columnspan=2, padx=10, pady=10, ipadx=10, ipady=10)
+
+	btn_back = tk.Button(deleteEvent_frame, text="Back", font=font, bg=button_bg, fg=button_fg, command=landing_page)
+	btn_back.grid(columnspan=2, padx=10, pady=10, ipadx=10, ipady=10)
+
+def landing_page():
+	destroy()
+	# Landing Page
+	landing_frame = tk.Frame(root)
+	landing_frame.pack()
+
+	# Title
+	title = tk.Label(landing_frame, text="Events Management System", font=heading_font)
+	title.pack(padx=10, pady=10)
+
+	# Buttons
+	btn_select = tk.Button(landing_frame, text="Select", font=font, bg=button_bg, fg=button_fg, command=showEvent)
+	btn_select.pack(padx=10, pady=10, ipadx=10, ipady=10)
+
+	btn_insert = tk.Button(landing_frame, text="Insert", font=font, bg=button_bg, fg=button_fg, command=newEvent)
+	btn_insert.pack(padx=10, pady=10, ipadx=10, ipady=10)
+
+	btn_update = tk.Button(landing_frame, text="Update", font=font, bg=button_bg, fg=button_fg, command=updateEvent)
+	btn_update.pack(padx=10, pady=10, ipadx=10, ipady=10)
+
+	btn_delete = tk.Button(landing_frame, text="Delete", font=font, bg=button_bg, fg=button_fg, command=deleteEvent)
+	btn_delete.pack(padx=10, pady=10, ipadx=10, ipady=10)
+
+	# Footer
+	footer = tk.Label(landing_frame, text="Developed by: Ayaan and Aldrin", font=font)
+	footer.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+landing_page()
+root.mainloop()
--- SQL/db.sql
+++ SQL/db.sql
@@ -1,81 +1,17 @@
--- Create a new database named "Library"
-CREATE DATABASE IF NOT EXISTS Library;
-
--- Switch to the "Library" database
-USE Library;
-
--- Create the "Book" table
-CREATE TABLE IF NOT EXISTS Books (
-	bookId INT PRIMARY KEY,
-	author VARCHAR(255),
-	title VARCHAR(255),
-	publishYear INT,
-	editionNo INT,
-	quantity INT,
-	shelfNo INT
+CREATE DATABASE Events;
+USE Events;
+CREATE Table Events (
+	id INT NOT NULL AUTO_INCREMENT,
+	title VARCHAR(255) NOT NULL,
+	description VARCHAR(255) NOT NULL,
+	date DATE NOT NULL,
+	fees INT NOT NULL,
+	PRIMARY KEY (id)
 );
 
--- Insert dummy data
-INSERT INTO Books VALUES
-	(1, "John Doe", "The Great Adventure", 2020, 1, 10, 101),
-	(2, "Jane Smith", "Coding 101", 2019, 2, 5, 102),
-	(3, "Alice Johnson", "Mystery Mansion", 2022, 1, 8, 103),
-	(4, "Bob Williams", "Data Science Handbook", 2021, 3, 12, 104),
-	(5, "Eva Davis", "History Unraveled", 2018, 2, 15, 105),
-	(6, "Ravi Patel", "The Silent Hills", 2017, 1, 7, 106),
-	(7, "Priya Sharma", "Code of Destiny", 2023, 2, 10, 107),
-	(8, "Amit Singh", "Infinite Connections", 2019, 1, 5, 108),
-	(9, "Kavita Verma", "Whispers of the Soul", 2020, 2, 8, 109),
-	(10, "Raj Kapoor", "The Forgotten Empire", 2021, 1, 12, 110),
-	(11, "Anita Joshi", "Programming Paradigm", 2018, 3, 15, 111),
-	(12, "Vikram Mehta", "Eternal Love", 2022, 2, 9, 112),
-	(13, "Suman Khanna", "Beyond the Horizon", 2016, 1, 11, 113),
-	(14, "Rahul Kumar", "The Quantum Mind", 2023, 3, 14, 114),
-	(15, "Mona Chopra", "Sands of Time", 2015, 2, 6, 115),
-	(16, "Deepak Sharma", "Data Analytics Explained", 2020, 1, 8, 116),
-	(17, "Neha Gupta", "The Enchanted Forest", 2022, 2, 13, 117),
-	(18, "Arun Kumar", "Web Development Unleashed", 2017, 1, 7, 118),
-	(19, "Sonia Verma", "In Pursuit of Happiness", 2019, 3, 10, 119),
-	(20, "Vivek Singh", "Mystical Realms", 2021, 1, 12, 120),
-	(21, "Pooja Kapoor", "Code of the Cosmos", 2018, 2, 15, 121),
-	(22, "Sanjay Mehta", "The Art of Programming", 2016, 1, 9, 122),
-	(23, "Anjali Saxena", "Echoes of Eternity", 2022, 3, 11, 123),
-	(24, "Rahul Verma", "The Lost Civilization", 2017, 2, 14, 124),
-	(25, "Sneha Sharma", "Journey to the Stars", 2019, 1, 6, 125),
-	(26, "Michael Johnson", "The Future of Artificial Intelligence", 2023, 1, 10, 201),
-	(27, "Sophie Müller", "Cybersecurity Essentials", 2022, 2, 8, 202),
-	(28, "Hiroshi Tanaka", "Quantum Computing: A Practical Guide", 2021, 1, 12, 203),
-	(29, "Olga Petrov", "Blockchain Revolution", 2020, 3, 15, 204),
-	(30, "Carlos Rodriguez", "The DevOps Handbook", 2019, 2, 9, 205),
-	(31, "Yuki Kato", "Python Mastery", 2018, 1, 11, 206),
-	(32, "Emma Smith", "Machine Learning for Beginners", 2017, 2, 14, 207),
-	(33, "Chen Wei", "Cloud Computing: The Complete Guide", 2022, 1, 7, 208),
-	(34, "Isabella Rossi", "Data Science in Action", 2016, 3, 10, 209),
-	(35, "Miguel Hernandez", "The Agile Manifesto", 2020, 1, 12, 210),
-	(36, "Aisha Khan", "UX Design Principles", 2019, 2, 15, 211),
-	(37, "Kenji Yamamoto", "Mobile App Development 101", 2021, 1, 8, 212),
-	(38, "Ananya Patel", "The Internet of Things Revolution", 2018, 2, 13, 213),
-	(39, "Sebastian Müller", "Artificial Neural Networks Explained", 2023, 1, 9, 214),
-	(40, "Maria Lopez", "Ethical Hacking and Penetration Testing", 2017, 2, 11, 215),
-	(41, "Elena Rodriguez", "The Enchanted Kingdom", 2019, 1, 10, 301),
-	(42, "Gregor Blackthorn", "Realm of Dragons", 2020, 2, 8, 302),
-	(43, "Luna Silvermoon", "The Elven Prophecy", 2021, 1, 12, 303),
-	(44, "Xander Darkblade", "Sorcery's Secret", 2018, 3, 15, 304),
-	(45, "Aria Nightshade", "Chronicles of the Mystic Isles", 2017, 2, 9, 305),
-	(46, "Thorn Ironheart", "The Shadow's Embrace", 2022, 1, 11, 306),
-	(47, "Isolde Moonshadow", "Eternal Nightfall", 2016, 2, 14, 307),
-	(48, "Gideon Frostfire", "Wizard's Awakening", 2023, 1, 7, 308),
-	(49, "Astrid Stormrider", "The Phoenix Crown", 2020, 3, 10, 309),
-	(50, "Soren Stormwind", "Tales of the Feywild", 2018, 2, 12, 310),
-	(51, "Lyra Moonstone", "The Crystal Labyrinth", 2021, 1, 15, 311),
-	(52, "Faelan Starlight", "Dragon's Flight", 2017, 2, 8, 312),
-	(53, "Eowyn Silverwind", "The Lost Scepter", 2019, 1, 13, 313),
-	(54, "Cedric Shadowcaster", "Whispers of the Ancients", 2022, 2, 10, 314),
-	(55, "Morgana Darkheart", "The Cursed Chronicles", 2016, 1, 9, 315),
-	(56, "Aerin Firestorm", "The Fey Court Conspiracy", 2023, 3, 11, 316),
-	(57, "Thalia Nightshade", "Serpent's Dance", 2017, 2, 14, 317),
-	(58, "Kael Sunblade", "Kingdom of Shadows", 2019, 1, 6, 318),
-	(59, "Selene Starfall", "The Astral Odyssey", 2020, 2, 11, 319),
-	(60, "Darian Frostbane", "The Elemental War", 2018, 3, 12, 320);
+INSERT INTO Events (title, description, date, fees) VALUES
+("Summer Music Festival","Annual Summer Music Festival!","2023-08-20", 50),
+("Soccer Championship", "World Famous Soccer Championship.", "2023-09-10", 30),
+("Cirque du Mystique", "Cirque du Mystique's mesmerizing performance.", "2023-08-28", 70);
 
-SELECT * FROM Books;
+SELECT * FROM Events;
